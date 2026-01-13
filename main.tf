4501753018e435ca5c5b8c6cf3677c0c7dceed08
Markcuz-199 <cheeshun97@hotmail.com>
2026-01-14 00:34:04 +0800

    Add Security Group to existing instance

--- main.tf
+++ main.tf
@@ -9,10 +9,45 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+# ---------------------------------------------------------
+# NEW SECTION: Security Group (The Firewall)
+# ---------------------------------------------------------
+resource "aws_security_group" "web_sg" {
+  name        = "web-server-sg"
+  description = "Allow SSH and HTTP"
+
+  # Allow SSH (Port 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTP (Port 80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outgoing traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # 2. Create the EC2 Instance (Resource)
 resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.ubuntu.id  # Keeps using your dynamic AMI
   instance_type = "t2.micro"
+
+  # LINK THE FIREWALL HERE:
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "Terraform-Demo-Instance"
